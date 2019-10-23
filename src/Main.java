579ff7a4f5ec5915504cb16024c4d3db859cedc8
xiaole1204 <zxc25773313@gmail.com>
2019-10-23 14:55:05 +0800

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,39 @@
-public class Main {
-
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        int sum = 0;
+//        for (int i = 1; i <= 100; i++) {
+//            sum=sum=i;
+//            System.out.println("This sum is" + sum + ".\n");
+//        }}
+//    }
+//import java.util.Scanner;
+//public class Main{
+//    public static void main(String[] args) {
+//        Scanner scn = new Scanner(System.in);
+//        int n = scn.nextInt();
+//        int sum = 0;
+//        for (int i = 3; i<=n;i+=3){
+//            sum += i;
+//        }
+//        System.out.println(sum);
+//    }
+//}
+import java.util.Scanner;
+public class Main{
     public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 1; i <= 100; i++) {
-            sum=sum=i;
-            System.out.println("This sum is" + sum + ".\n");
-        }}
+        Scanner scn = new Scanner(System.in);
+        float v1 = scn.nextFloat();
+        float maxVal = v1;
+        float minVal = v1;
+        for (int i = 1 ; i <10;i++){
+            v1 = scn.nextFloat();
+            if (maxVal<v1){
+                maxVal=v1;
+            } else if (minVal>v1){
+                minVal=v1;
+            }
+        }
+        System.out.println(v1);
     }
+}
