7d7d271dea69ef4461a56cb6bd695a1134dbabb2
xiaole1204 <zxc25773313@gmail.com>
2019-10-23 15:38:06 +0800

    First commit

--- src/Main.java
+++ src/Main.java
@@ -19,22 +19,42 @@
 //        System.out.println(sum);
 //    }
 //}
+//import java.util.Scanner;
+//public class Main{
+//    public static void main(String[] args) {
+//        Scanner scn = new Scanner(System.in);
+//        float v1 = scn.nextFloat();
+//        float maxVal = v1;
+//        float minVal = v1;
+//        for (int i = 1 ; i <10;i++){
+//            v1 = scn.nextFloat();
+//            if (maxVal<v1){
+//                maxVal=v1;
+//            } else if (minVal>v1){
+//                minVal=v1;
+//            }
+//        }
+//        System.out.printf("%.2f" , maxVal);
+//        System.out.printf("%.2f" , minVal);
+//    }
+//}
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        float v1 = scn.nextFloat();
-        float maxVal = v1;
-        float minVal = v1;
-        for (int i = 1 ; i <10;i++){
-            v1 = scn.nextFloat();
-            if (maxVal<v1){
-                maxVal=v1;
-            } else if (minVal>v1){
-                minVal=v1;
+        int n = scn.nextInt();
+        int m = n / 2;
+        boolean flag = true;
+        for (int i = 2 ; i<=m;i++){
+            if (n% i == 0){
+                flag = false;
+                break;
             }
         }
-        System.out.printf("%.2f" , maxVal);
-        System.out.printf("%.2f" , minVal);
+        if (flag){
+            System.out.println("Yes");
+        } else {
+            System.out.println("no");
+        }
     }
 }
