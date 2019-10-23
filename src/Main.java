9f81baf9198b6a9ddd2c3ef66bda7fe688926341
xiaole1204 <zxc25773313@gmail.com>
2019-10-23 15:54:24 +0800

    First commit

--- src/Main.java
+++ src/Main.java
@@ -38,23 +38,35 @@
 //        System.out.printf("%.2f" , minVal);
 //    }
 //}
+//import java.util.Scanner;
+//public class Main{
+//    public static void main(String[] args) {
+//        Scanner scn = new Scanner(System.in);
+//        int n = scn.nextInt();
+//        int m = n / 2;
+//        boolean flag = true;
+//        for (int i = 2 ; i<=m;i++){
+//            if (n% i == 0){
+//                flag = false;
+//                break;
+//            }
+//        }
+//        if (flag){
+//            System.out.println("Yes");
+//        } else {
+//            System.out.println("no");
+//        }
+//    }
+//}
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int m = n / 2;
-        boolean flag = true;
-        for (int i = 2 ; i<=m;i++){
-            if (n% i == 0){
-                flag = false;
-                break;
-            }
+        int sum = 0;
+        for (int i = 1; i <=n;i++){
+            sum = sum+i*(i+1);
         }
-        if (flag){
-            System.out.println("Yes");
-        } else {
-            System.out.println("no");
-        }
+        System.out.println(sum);
     }
 }
